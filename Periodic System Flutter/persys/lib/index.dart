ed693bf0db89055b2f3c27a3190f750a84f56a5d
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 <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>
2021-11-05 14:39:37 +0100

    SHows all rows in a table

--- Periodic System Flutter/persys/lib/index.dart
+++ Periodic System Flutter/persys/lib/index.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'rows.dart';
 import 'cell.dart';
 
 class Index extends StatelessWidget {
@@ -9,17 +9,26 @@ class Index extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: const Color.fromRGBO(52, 56, 75, 1),
-        child: Center(
-          child: Container(
-            width: 70,
-            height: 70,
-            color: Colors.green,
-            child: const Cell(),
-          ),
+      backgroundColor: const Color.fromRGBO(52, 56, 75, 1),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: MediaQuery.of(context).size.height * 0.9,
+          color: Colors.green,
+          child: Table(
+            children: [
+              row1(),
+              row2(),
+              row3(),
+              row4(),
+              row5(),
+              row6(),
+              row7(),
+              paddingRow(),
+              row8(),
+              row9(),
+            ],
+          )
         ),
       ),
     );
