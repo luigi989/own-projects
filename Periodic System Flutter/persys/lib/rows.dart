188de9b09a18e153ea7a952f80deb484e448cfa1
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 <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>
2021-11-05 14:38:50 +0100

    Added all rows and cells

---
+++ Periodic System Flutter/persys/lib/rows.dart
@@ -0,0 +1,270 @@
+import 'package:flutter/cupertino.dart';
+import 'cell.dart';
+
+TableRow row1() {
+  return (
+       TableRow(
+         children: [
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+        ],
+      )
+  );
+}
+
+TableRow row2() {
+  return(
+      TableRow(
+        children: [
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+        ],
+      )
+  );
+}
+
+TableRow row3() {
+  return(
+      TableRow(
+        children: [
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+        ],
+      )
+  );
+}
+
+TableRow row4() {
+  return(
+      const TableRow(
+        children: [
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+        ],
+      )
+  );
+}
+
+TableRow row5() {
+  return(
+      const TableRow(
+        children: [
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+        ],
+      )
+  );
+}
+
+TableRow row6() {
+  return(
+      const TableRow(
+        children: [
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+        ],
+      )
+  );
+}
+
+TableRow row7() {
+  return(
+      const TableRow(
+        children: [
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: EdgeInsets.all(5),child: Cell()),
+        ],
+      )
+  );
+}
+
+TableRow paddingRow() {
+  return const TableRow(
+    children: [
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+      SizedBox(height: 20),
+    ]
+  );
+}
+
+TableRow row8() {
+  return(
+      TableRow(
+        children: [
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+        ],
+      )
+  );
+}
+
+TableRow row9() {
+  return(
+      TableRow(
+        children: [
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          const Padding(padding: EdgeInsets.all(5),child: Cell()),
+          Padding(padding: const EdgeInsets.all(5),child: Container()),
+        ],
+      )
+  );
+}
