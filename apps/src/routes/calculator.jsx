79047f2712e8a4141d00df959e8c9dfbd0a3b8ae
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 <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>
2021-12-30 21:48:34 +0100

    can now use the four mathematic operations

--- apps/src/routes/calculator.jsx
+++ apps/src/routes/calculator.jsx
@@ -1,29 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../css/calculator.css';
 
 /*
 TODO: After clearing last number, number1 or number2 are not updated with the cleared number.
 */
 export default function Calculator() {
-  const [sum, setSum] = useState();
   const [number1, setNumber1] = useState(0);
   const [number2, setNumber2] = useState(0);
-  const [operator, setOperator] = useState();
+  const [total, setTotal] = useState(0);
+
+  const [operator, setOperator] = useState("empty");
   const [operatorSet, setOperatorSet] = useState(false);
   const [numberDisplayed, setNumberDisplayed] = useState(0);
 
-  const saveNumber = (number) => {
-    number = removeZeroIfExists(number);
-    console.log(number);
+  const printInfo = () => {
+    console.log(number1 + ": " + number2);
+    console.log(operator + ": " + operatorSet);
+  }
 
-    if(operatorSet) {
-      setNumber2(number);
-    } else {
+  const saveNumber = (number) => {
+    number = parseInt(removeZeroIfExists(number));
+    console.log(number);
+    if(!operatorSet) {
       setNumber1(number);
+    } else {
+      setNumber2(number);
     }
     setNumberDisplayed(number);
-    // console.log(number1 + ": " + number2);
-    // console.log(operator + ": " + operatorSet);
+  }
+  
+  const num1orNum2 = () => {
+    if(!operatorSet) {
+      return number1;
+    } else {
+      return number2;
+    }
   }
 
   const removeLastNumber = () => {
@@ -33,13 +44,12 @@ export default function Calculator() {
       setNumberDisplayed(0);
     }
   }
+
   const removeZeroIfExists = (number) => {
     var pattern = new RegExp("^0");
     if(pattern.test(number)) {
-      console.log(number);
       return number.slice(1);
     } else {
-      console.log("not work");
       return number;
     }
   }
@@ -60,14 +70,32 @@ export default function Calculator() {
   }
 
   const calculate = () => {
-    if(operatorSet && number1 > 0 && number2 > 0) {
-      setSum(number1 + number2);
-      setNumberDisplayed(number1 + number2);
-      console.log(sum);
+    if(operatorSet && (number1 > 0 || number2 > 0)) {
+      switch(operator) {
+        case '+':
+          setTotal(number1 + number2);
+          break;
+        case '-':
+          setTotal(number1 - number2);
+          break;
+        case '*':
+          setTotal(number1 * number2);
+          break;
+        case '/': 
+        setTotal(number1 / number2);
+          break;
+        default:
+          break;
+      }
     } else {
       console.log("Something went wrong");
     }
   }
+
+  useEffect(() => {
+    setNumberDisplayed(total);
+    console.log(total);
+  }, [total, setTotal])
 
   return (
     <div className="content text-light">
@@ -76,22 +104,25 @@ export default function Calculator() {
         <div className='grid-5x4'>
           <button className='grid-start' onClick={clearNumbers}>AC</button>
           <button onClick={removeLastNumber}>C</button>
-          <button>/</button>
+          <button onClick={e => saveOperator(e.target.textContent)}>/</button>
 
-          <button onClick={e => saveNumber(number1 + e.target.textContent)}>9</button>
-          <button>8</button>
-          <button>7</button>
-          <button>*</button>
-          <button>6</button>
-          <button>5</button>
-          <button>4</button>
-          <button>-</button>
-          <button>3</button>
-          <button>2</button>
-          <button>1</button>
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>9</button>
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>8</button>
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>7</button>
+          <button onClick={e => saveOperator(e.target.textContent)}>*</button>
+
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>6</button>
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>5</button>
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>4</button>
+
+          <button onClick={e => saveOperator(e.target.textContent)}>-</button>
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>3</button>
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>2</button>
+          <button onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>1</button>
           <button onClick={e => saveOperator(e.target.textContent)}>+</button>
-          <button className='grid-start'>0</button>
-          <button>,</button>
+
+          <button className='grid-start' onClick={e => saveNumber(num1orNum2() + e.target.textContent)}>0</button>
+          <button>.</button>
           <button onClick={calculate}>=</button>
         </div>
     </div>
