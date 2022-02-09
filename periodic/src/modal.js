415c0eba26f25a334e6fdfdf95d3ae6d4bf6c224
ludde <ludde.lindahl@gmail.com>
2022-02-09 15:03:45 +0100

    added folder of element electron shells

--- periodic/src/modal.js
+++ periodic/src/modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Col, Container, ListGroup, Modal, Row } from 'react-bootstrap';
 
 function ElementModal( {closeModal, data} ) {
-
+console.log(`https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Electron_shell_${data.number}_${data.name}_-_no_label.svg/800px-Electron_shell_${data.number}_${data.name}_-_no_label.svg.png`);
     return (
         <>
           <Modal className="modal" show={true} onHide={closeModal}>
@@ -22,7 +22,10 @@ function ElementModal( {closeModal, data} ) {
                 </ListGroup>
                 </Col>
                 <Col xs={6} md={4} className="text-light">
-                  marker for image
+                  <img 
+                    src={`https://upload.wikimedia.org/wikipedia/commons/2/29/Electron_shell_006_Carbon.svg`}
+                    alt={`${data.name} atomic model`}
+                  ></img>
                 </Col>
               </Row>
               <Row className="p-3 text-light">
