0d3bd11b91ce98f8b15fd5b5803b4706b5f9b04b
ludde <ludde.lindahl@gmail.com>
2022-02-09 22:26:30 +0100

    added images and imporved popup

--- periodic/src/modal.js
+++ periodic/src/modal.js
@@ -2,34 +2,35 @@ import React from 'react';
 import { Col, Container, ListGroup, Modal, Row } from 'react-bootstrap';
 
 function ElementModal( {closeModal, data} ) {
-console.log(`https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Electron_shell_${data.number}_${data.name}_-_no_label.svg/800px-Electron_shell_${data.number}_${data.name}_-_no_label.svg.png`);
     return (
         <>
-          <Modal className="modal" show={true} onHide={closeModal}>
-            <Modal.Header closeButton>
+          <Modal className="modal" size="lg" show={true} onHide={closeModal}>
+            <Modal.Header className="" closeButton>
               <Modal.Title className="w-100 text-center text-light">{data.name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
               <Row>
-                <Col xs={12} md={8}>
+                <Col xs={12} md={6}>
                 <ListGroup>
-                  <ListGroup.Item variant="dark">Appearance: {data.appearance}</ListGroup.Item>
-                  <ListGroup.Item variant="dark">Phase: {data.phase}</ListGroup.Item>
-                  <ListGroup.Item variant="dark">Atomic mass: {data.atomic_mass} u</ListGroup.Item>
-                  <ListGroup.Item variant="dark">Boil point: {data.boil} &#8490;</ListGroup.Item>
-                  <ListGroup.Item variant="dark">Melting point: {data.melt} &#8490;</ListGroup.Item>
-                  <ListGroup.Item variant="dark">Density: {data.density} g/L</ListGroup.Item>
+                  <ListGroup.Item className='text-light' variant="dark">Appearance: {data.appearance}</ListGroup.Item>
+                  <ListGroup.Item className='text-light' variant="dark">Phase: {data.phase}</ListGroup.Item>
+                  <ListGroup.Item className='text-light' variant="dark">Atomic mass: {data.atomic_mass} u</ListGroup.Item>
+                  <ListGroup.Item className='text-light' variant="dark">Boil point: {data.boil} &#8490;</ListGroup.Item>
+                  <ListGroup.Item className='text-light' variant="dark">Melting point: {data.melt} &#8490;</ListGroup.Item>
+                  <ListGroup.Item className='text-light' variant="dark">Density: {data.density} g/L</ListGroup.Item>
                 </ListGroup>
                 </Col>
-                <Col xs={6} md={4} className="text-light">
+                <Col xs={12} md={6} 
+                    className="text-light d-flex flex-column 
+                    justify-content-center align-items-center">
                   <img 
-                    className='w-100'
+                    className='w-50 p-2'
                     src={ require(`./images/${data.name}.png`).default }
                     alt={`${data.name} atomic model`}
                   ></img>
                 </Col>
               </Row>
-              <Row className="p-3 text-light">
+              <Row className="desc m-2 mt-3 p-2 text-light align-middle">
                 {data.summary}
               </Row>
 
