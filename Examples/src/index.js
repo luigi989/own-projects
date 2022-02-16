6846cb9096df6650f2930eecb61e50937debcd9d
ludde <ludde.lindahl@gmail.com>
2022-02-16 19:49:32 +0100

    Fixed layout issue

--- Examples/src/index.js
+++ Examples/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
+
 import App from './App';
+import Sidebar from './sidebar'
 import LoginForm from './login_forms/login_form';
 import LoginFormTrans from './login_forms/login_form_trans';
-import Sidebar from './sidebar'
-
+import SearchBox from './misc/searchbox';
 
 const rootElement = document.getElementById('root')
 render(
@@ -14,11 +15,12 @@ render(
     <div id="outer-container" className='h-full'>
       <Sidebar outerContainerId={'outer-container'} pageWrapId={'page-wrap'} />
       {/* < Header /> */}
-      <div id='page-wrap'>
+      <div id='page-wrap' className='flex h-full'>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="loginForm" element={<LoginForm />} />
           <Route path="loginFormTrans" element={<LoginFormTrans />} />
+          <Route path='searchbox' element={<SearchBox />} />
         </Routes>
       </div>
     </div>
