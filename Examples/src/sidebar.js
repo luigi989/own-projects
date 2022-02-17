19a71439c33fbee7d2c2b94bcbcc2f9374340369
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 <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>
2022-02-17 10:50:24 +0100

    Add submenues and new menu entry

--- Examples/src/sidebar.js
+++ Examples/src/sidebar.js
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { NavLink } from 'react-router-dom';
 
 import './sidebar.css';
 
 import { FaPlus } from 'react-icons/fa';
+import { FaMinus } from 'react-icons/fa';
 
 
 const Sidebar = ({ outerContainerId, pageWrapId }) => {
+  const [cardsOpen, setCardsOpen] = useState(false);
+  const [loginFormsOpen, setLoginFormsOpen] = useState(false);
+
   return (
     <Menu pageWrapId={pageWrapId} outerContainerId={outerContainerId}>
         <NavLink to="/" 
                  className={({ isActive }) => (isActive ? 'linkActive' : 'linkInactive')}>
                 Home
         </NavLink>
-        <NavLink to="/loginForm" 
-                 className={({ isActive }) => (isActive ? 'linkActive' : 'linkInactive')}>
-                Login form
-        </NavLink>
-        <NavLink to="/loginFormTrans" 
-                 className={({ isActive }) => (isActive ? 'linkActive' : 'linkInactive')}>
-                Transparent login form
-        </NavLink>
-        <NavLink to="/searchbox" 
-                 className={({ isActive }) => (isActive ? 'linkActive' : 'linkInactive')}>
-                Searchbox
-        </NavLink>
-        <NavLink to="/profilecard">
-               <div className='flex items-center'>
-                    <span className=''>Profile cards</span>
-                    <FaPlus className='text-xs ml-2'/>
-               </div>
-        </NavLink>
+        <div>
+          <div className='flex items-center cursor-pointer' onClick={() => setLoginFormsOpen(!loginFormsOpen)}>
+            <span className=''>Login forms</span>
+            {loginFormsOpen ? <FaMinus className='text-xs ml-2'/> : <FaPlus className='text-xs ml-2'/>}
+          </div>
+        </div>
+        {loginFormsOpen ? 
+        <div>
+          <div className='flex flex-col ml-4'>
+            <NavLink to="/loginForm">Login form</NavLink>
+            <NavLink to="/loginFormTrans">Transparent login form</NavLink>
+          </div>
+        </div> : null}
+
+        <NavLink to="/searchbox">Searchbox</NavLink>
+
+        <div>
+          <div className='flex items-center cursor-pointer' onClick={() => setCardsOpen(!cardsOpen)}>
+            <span className=''>Profile cards</span>
+            {cardsOpen ? <FaMinus className='text-xs ml-2'/> : <FaPlus className='text-xs ml-2'/>}
+          </div>
+        </div>
+        {cardsOpen ? 
+        <div>
+          <div className='flex flex-col ml-4'>
+            <NavLink to="/profilecard" className=''>Profile card 1</NavLink>
+            <NavLink to="/profilecard2" className=''>Profile card 2</NavLink>
+          </div>
+        </div> : null}
     </Menu>
   );
 };
