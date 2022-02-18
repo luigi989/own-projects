e8120f1b02622c6968c8451fbb40ae89cdea4d53
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 <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>
2022-02-19 00:32:40 +0100

    Add jsx version

---
+++ Examples/src/button_effects/neon_effect.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className='flex-1 flex justify-center items-center'>
+        <div className='flex-col scale-75 md:scale-100'>
+            <div>
+                <h1>Blue</h1>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Neon Button
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
