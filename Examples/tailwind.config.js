da690cfa1fe08620aa645e6a1c9dc244b3bbb1f6
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 <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>
2022-02-19 00:30:35 +0100

    some changes

--- Examples/tailwind.config.js
+++ Examples/tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        move: 'move 2s linear infinite', 
+      },
+      keyframes: {
+        move: {
+          '25%': { 'background-position': 'left bottom' },
+          '50%': { 'background-position': 'right bottom' },
+          '75%': { 'background-position': 'right top' },
+          '100%': { 'background-position': 'left top' }
+        },
+        wiggle: {
+          '0%, 100%': { transform: 'translateX(25%)' },
+          '50%': { transform: 'translateY(25%)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
