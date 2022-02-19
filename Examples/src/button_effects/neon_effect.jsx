e45d752c2c06b7d305a6953c9ea07cc38dd9c22d
ludde <ludde.lindahl@gmail.com>
2022-02-19 01:43:51 +0100

    Add tailwind css

--- Examples/src/button_effects/neon_effect.jsx
+++ Examples/src/button_effects/neon_effect.jsx
@@ -1,14 +1,30 @@
+// import './neon_effect.css';
+
 function App() {
   return (
-    <div className='flex-1 flex justify-center items-center'>
+    <div className='flex-1 flex justify-center items-center bg-[#031321]'>
         <div className='flex-col scale-75 md:scale-100'>
-            <div>
-                <h1>Blue</h1>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                Neon Button
+            <div className="m-3 border-solid border-2 border-[#255784]
+                            text-[#255784]">
+                <h1 className="absolute top-0 left-7 px-2 bg-[#031321] 
+                               uppercase text-base">Blue</h1>
+                <button className="inline-block mx-10 my-6 uppercase text-xl tracking-wider
+                                   transition duration-200">
+                    <span className="absolute block h-[2px] top-0 w-full l-[-100%] hover:l-full
+                                     bg-gradient-to-r from-transparent to-[#255784]
+                                    transition duration-1000"></span>
+                    <span className="absolute block "></span>
+                    <span className="absolute block "></span>
+                    <span className="absolute block "></span>
+                    Neon Button
+                </button>
+                <button className="inline-block mx-10 my-4 uppercase text-xl tracking-wider">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Neon Button
+                </button>
             </div>
         </div>
     </div>
