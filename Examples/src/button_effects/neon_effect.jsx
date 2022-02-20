a402ea22a4aafe8e80ec25e792d32fe0636ff178
ludde <ludde.lindahl@gmail.com>
2022-02-21 00:08:45 +0100

    Almost fixed neon effect animation

--- Examples/src/button_effects/neon_effect.jsx
+++ Examples/src/button_effects/neon_effect.jsx
@@ -2,31 +2,36 @@
 
 function App() {
   return (
-    <div className='flex-1 flex justify-center items-center bg-[#031321]'>
-        <div className='flex-col scale-75 md:scale-100'>
+    <div className='flex-1 flex flex-col justify-center items-center bg-[#031321]'>
+        <div className='scale-75 md:scale-100'>
             <div className="m-3 border-solid border-2 border-[#255784]
-                            text-[#255784]">
+                            text-[#255784] box-border p-6">
                 <h1 className="absolute top-0 left-7 px-2 bg-[#031321] 
                                uppercase text-base">Blue</h1>
-                <button className="inline-block mx-10 my-6 uppercase text-xl tracking-wider
-                                   transition duration-200">
-                    <span className="absolute block h-[2px] top-0 w-full l-[-100%] hover:l-full
-                                     bg-gradient-to-r from-transparent to-[#255784]
-                                    transition duration-1000"></span>
-                    <span className="absolute block "></span>
-                    <span className="absolute block "></span>
-                    <span className="absolute block "></span>
+                <div className="relative group px-4 py-4 uppercase text-xl tracking-wider">
+                    <span className="absolute h-[2px] top-0 left-0 w-0 
+                                    group-hover:w-full group-hover:duration-500
+                                    bg-gradient-to-r from-transparent to-[#255784]"></span>
+                    <span className="absolute h-[2px] bottom-0 right-0 w-0 group-hover:w-full 
+                                    group-hover:duration-500   
+                                    bg-gradient-to-l from-transparent to-[#255784]"></span>
                     Neon Button
-                </button>
-                <button className="inline-block mx-10 my-4 uppercase text-xl tracking-wider">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    Neon Button
-                </button>
+                </div>
             </div>
         </div>
+
+        <nav className="flex sm:justify-center space-x-4">
+          {[
+            ['Home', '[#255784]'],
+            ['Team', '[#055205]'],
+            ['Projects', '[#c51313]'],
+            ['Reports', '[#743ad5]'],
+          ].map(([title, color]) => (
+            <div className={`rounded-lg px-3 py-2 text-white
+                                     font-medium hover:bg-slate-100 hover:text-slate-900
+                                     bg-${color}`}>{title}</div>
+          ))}
+        </nav>
     </div>
   );
 }
