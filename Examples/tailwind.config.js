3c39ef9e0be8fba5ffd9d3e2711f4d80b25d64b8
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 <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>
2022-02-21 14:33:34 +0100

    Change animations

--- Examples/tailwind.config.js
+++ Examples/tailwind.config.js
@@ -5,16 +5,26 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        move: 'move_right 1s linear', 
+        moveWidth: 'moveWidth 0.5s linear', 
+        moveWidthInfinite: 'moveWidth 0.5s linear infinite', 
+        moveHeight: 'moveHeight 0.5s linear', 
+        moveHeightInfinite: 'moveHeight 0.5s linear infinite', 
       },
       keyframes: {
-        move_right: {
+        moveWidth: {
           '0%': { width: '0%' },
           '99%': { width: '100%' },
           '100%': { width: '0%' },
+        }, 
+        moveHeight: {
+          '0%': { heiight: '0%' },
+          '99%': { height: '100%' },
+          '100%': { height: '0%' },
         }
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animation-delay"),
+  ],
 }
