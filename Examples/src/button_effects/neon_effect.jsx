855a2a3aae89b3317fc7cee10ad17fe873844e5d
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 <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>
2022-02-21 12:33:07 +0100

    Add hover neon glow effect in all colors

--- Examples/src/button_effects/neon_effect.jsx
+++ Examples/src/button_effects/neon_effect.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className='flex-1 flex flex-col justify-center items-center bg-[#031321]'>
         <div className='flex flex-col scale-75 md:scale-100'>
             {/* Blue */}
-            <div className="relative m-3 border-solid border-2 border-[#255784]
+            <div className="flex relative m-3 border-solid border-2 border-[#255784]
                             text-[#255784] box-border p-6">
                 <h1 className="absolute top-[-12%] left-3 px-2 bg-[#031321] 
                                uppercase text-base">Blue</h1>
@@ -18,9 +18,14 @@ function App() {
                                     bg-gradient-to-l from-transparent to-[#255784]"></span>
                     Neon Button
                 </div>
+                
+                <div className="relative px-4 py-4 uppercase text-[#255784] text-xl tracking-wider
+                              hover:bg-black hover:outline hover:outline-2 hover:outline-[#255784]">
+                        Neon Button
+                </div>
             </div>
             {/* Green */}
-            <div className="relative m-3 border-solid border-2 border-[#055205]
+            <div className="flex relative m-3 border-solid border-2 border-[#055205]
                             text-[#0c8612] box-border p-6">
                 <h1 className="absolute top-[-12%] left-3 px-2 bg-[#031321] 
                                uppercase text-base">Green</h1>
@@ -33,9 +38,14 @@ function App() {
                                     bg-gradient-to-l from-transparent to-[#0c8612]"></span>
                     Neon Button
                 </div>
+
+                <div className="relative px-4 py-4 uppercase text-[#0c8612] text-xl tracking-wider
+                              hover:bg-black hover:outline hover:outline-2 hover:outline-[#055205]">
+                        Neon Button
+                </div>
             </div>
             {/* Red */}
-            <div className="relative m-3 border-solid border-2 border-[#8a1616]
+            <div className=" flex relative m-3 border-solid border-2 border-[#8a1616]
                             text-[#c51313] box-border p-6">
                 <h1 className="absolute top-[-12%] left-3 px-2 bg-[#031321] 
                                uppercase text-base">Red</h1>
@@ -48,9 +58,14 @@ function App() {
                                     bg-gradient-to-l from-transparent to-[#c51313]"></span>
                     Neon Button
                 </div>
+
+                <div className="relative px-4 py-4 uppercase text-[#c51313] text-xl tracking-wider
+                              hover:bg-black hover:outline hover:outline-2 hover:outline-[#c51313]">
+                        Neon Button
+                </div>
             </div>
             {/* Black */}
-            <div className="relative m-3 border-solid border-2 border-black
+            <div className="flex items-center relative m-3 border-solid border-2 border-black
                             text-[#585353] box-border p-6">
                 <h1 className="absolute top-[-12%] left-3 px-2 bg-[#031321] 
                                uppercase text-base">Black</h1>
@@ -63,33 +78,40 @@ function App() {
                                     bg-gradient-to-l from-transparent to-black"></span>
                     Neon Button
                 </div>
+
+                <div className="relative px-4 py-4 uppercase text-[#585353] text-xl tracking-wider
+                              hover:bg-black hover:outline hover:outline-2 hover:outline-[#585353]">
+                        Neon Button
+                </div>
             </div>
             {/* Multicolor */}
-            <div className="flex items-center relative p-1 bg-gradient-to-r from-[#743ad5] to-red-700">
+            <div className="flex items-center relative m-3   p-px bg-gradient-to-r from-[#743ad5] to-red-700">
               <h1 className="absolute top-[-10%] left-3 px-2 bg-[#031321] 
                              text-[#743ad5] uppercase text-base">Multicolor</h1>
-              <div className="bg-[#031321]
-                              text-[#743ad5] p-5">
-                  <div className="relative group px-4 py-4 uppercase text-[#585353] text-xl tracking-wider">
-                      <span className="absolute h-[2px] top-0 left-0 w-0 
-                                      group-hover:w-full group-hover:duration-500
-                                      bg-gradient-to-r from-[#743ad5] to-red-700"></span>
-                      <span className="absolute h-[2px] bottom-0 right-0 w-0 group-hover:w-full 
-                                      group-hover:duration-500   
-                                      bg-gradient-to-l from-[#743ad5] to-red-700"></span>
-                      Neon Button
-                  </div>
-              </div>
-
-              <div className="bg-[#031321] p-5">
-                <div className="group relative bg-[#031321] hover:bg-gradient-to-l from-[#743ad5] to-red-700
-                                text-[#743ad5] p-px">
-                    <div className="relative px-4 py-4 uppercase text-[#585353] text-xl tracking-wider
-                                    group-hover:text-[#585353] group-hover:bg-black ">
+              <div className="bg-[#031321] flex">
+                <div className="text-[#743ad5] p-5">
+                    <div className="relative group px-4 py-4 uppercase text-[#585353] text-xl tracking-wider">
+                        <span className="absolute h-[2px] top-0 left-0 w-0 
+                                        group-hover:w-full group-hover:duration-500
+                                        bg-gradient-to-r from-[#743ad5] to-red-700"></span>
+                        <span className="absolute h-[2px] bottom-0 right-0 w-0 group-hover:w-full 
+                                        group-hover:duration-500   
+                                        bg-gradient-to-l from-[#743ad5] to-red-700"></span>
                         Neon Button
                     </div>
                 </div>
+
+                <div className="p-5">
+                  <div className="group relative bg-[#031321] hover:bg-gradient-to-l from-[#743ad5] to-red-700
+                                  text-[#743ad5] p-px">
+                      <div className="relative px-4 py-4 uppercase text-[#585353] text-xl tracking-wider
+                                      group-hover:text-[#585353] group-hover:bg-black ">
+                          Neon Button
+                      </div>
+                  </div>
+                </div>
               </div>
+
             </div>
         </div>
 
