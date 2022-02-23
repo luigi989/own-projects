297d7f72c21f519bc340410cb4af8f548333bb4e
ludde <ludde.lindahl@gmail.com>
2022-02-23 17:40:37 +0100

    Fix nasty bug and add the last hover effects.
    A bug that make the code work on one computer but not other computers.
    Tailwind css can't be split up in parts, where one part is a variable.

--- Examples/src/button_effects/hover_effect.jsx
+++ Examples/src/button_effects/hover_effect.jsx
@@ -11,40 +11,69 @@ function HoverEffect() {
           <div className='flex flex-col scale-75 md:scale-100 bg-slate-300 rounded-2xl p-3'>
               <div className="flex flex-col m-6">
                 {[
-                  ['[#255784]'],['[#ca1f1f]'],['[#92530b]']
-                ].map(([color]) => (
-                    <div className={`relative flex gap-20 m-4 text-5xl text-${color}`}>
+                  ['border-[#255784]', 'text-[#255784]'],
+                  ['border-[#ca1f1f]', 'text-[#ca1f1f]'],
+                  ['border-[#92530b]', 'text-[#92530b]']
+                ].map(([borderColor, textColor]) => (
+                    <div className={`relative flex gap-20 m-4 text-5xl ${textColor}`}>
                         <div className='group relative cursor-pointer'>
-                            {[['top-0', 'border-t-4', 'w-full', '-skew-y'], 
-                              ['right-0', 'border-r-4', 'h-full', 'skew-x'],
-                              ['bottom-0', 'border-b-4', 'w-full', '-skew-y'], 
-                              ['left-0', 'border-l-4', 'h-full', 'skew-x']
+                            {[['top-0', 'group-hover:border-t-4', 'group-hover:w-full', 'group-hover:-skew-y-12'], 
+                              ['right-0', 'group-hover:border-r-4', 'group-hover:h-full', 'group-hover:skew-x-12'],
+                              ['bottom-0', 'group-hover:border-b-4', 'group-hover:w-full', 'group-hover:-skew-y-12'], 
+                              ['left-0', 'group-hover:border-l-4', 'group-hover:h-full', 'group-hover:skew-x-12']
                             ].map(([position, border, size, skew]) => (
-                                <span className={`absolute ${position} border-${color} duration-300 
-                                                group-hover:border-solid group-hover:${size} 
-                                                group-hover:${border} group-hover:${skew}-12 `}>
+                                <span className={`absolute ${position} ${borderColor} duration-300 
+                                                group-hover:border-solid ${size} 
+                                                ${border} ${skew}`}>
                                 </span>
                             ))}
                             <FaFacebookF className='group-hover:scale-75 m-3 duration-300'/>
                         </div>
 
                         <div className='group relative cursor-pointer'>
-                            {[['top-0', 'border-t-4', 'w-full', 'skew-y'], 
-                              ['right-0', 'border-r-4', 'h-full', 'skew-x'],
-                              ['bottom-0', 'border-b-4', 'w-full', 'skew-y'], 
-                              ['left-0', 'border-l-4', 'h-full', 'skew-x']
-                            ].map(([position, border, size, skew]) => (
-                                <span className={`absolute ${position} border-${color} duration-300 
-                                                group-hover:border-solid group-hover:${size} 
-                                                group-hover:border-4`}>
+                            {[['top-0', 'group-hover:border-t-8', 'group-hover:w-full'], 
+                              ['right-0', 'group-hover:border-r-4', 'group-hover:h-full'],
+                              ['bottom-0', 'group-hover:border-b-8', 'group-hover:w-full'], 
+                              ['left-0', 'group-hover:border-l-4', 'group-hover:h-full']
+                            ].map(([position, border, sizeHover]) => (
+                                <span className={`absolute ${position} ${sizeHover} duration-300 
+                                                group-hover:border-solid ${border} ${borderColor}`}>
                                 </span>
                             ))}
                             <FaTwitter className='group-hover:scale-75 m-3 duration-300'/>
                         </div>
-                        <FaInstagram />
-                        <FaYoutube />
-                        <FaGithub />
-                        <FaLinkedin />
+                        <div className='group relative cursor-pointer'>
+                            <span className={`absolute w-full h-full ${borderColor} duration-300 
+                                            group-hover:border-solid rounded-full
+                                            group-hover:border-t-4 group-hover:border-l-4`}>
+                            </span>
+                            <FaInstagram className='group-hover:scale-75 m-3 duration-300'/>
+                        </div>
+                        <div className='group relative cursor-pointer'>
+                            <span className={`absolute w-full h-full ${borderColor} duration-300 
+                                            group-hover:border-solid rounded-2xl
+                                            group-hover:border-4`}>
+                            </span>
+                            <FaYoutube className='group-hover:scale-75 m-3 duration-300'/>
+                        </div>
+                        <div className='group relative cursor-pointer'>
+                            <span className={`absolute w-full h-full ${borderColor} duration-300 
+                                            group-hover:border-solid
+                                            group-hover:border-2`}>
+                            </span>
+                            <span className={`absolute w-full h-full ${borderColor} duration-300 
+                                            group-hover:border-solid group-hover:rotate-45
+                                            group-hover:border-4 group-hover:scale-[0.7]`}>
+                            </span>
+                            <FaGithub className='group-hover:scale-75 m-3 duration-300'/>
+                        </div>
+                        <div className='group relative cursor-pointer'>
+                            <span className={`absolute w-1/5 h-1/5 ${borderColor} duration-300 
+                                            group-hover:border-solid rounded-full
+                                            group-hover:border-2`}>
+                            </span>
+                            <FaLinkedin className='group-hover:scale-75 m-3 duration-300'/>
+                        </div>
                     </div>
                 ))}
               </div>
