63c7c8228a5c2422e51c7c7bc3d3eb659fbe328c
ludde <ludde.lindahl@gmail.com>
2022-02-23 17:34:46 +0100

    Add custom colors

--- Examples/tailwind.config.js
+++ Examples/tailwind.config.js
@@ -2,13 +2,18 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
+  theme: {    
     extend: {
       animation: {
         moveWidth: 'moveWidth 0.5s linear', 
         moveWidthInfinite: 'moveWidth 0.5s linear infinite', 
         moveHeight: 'moveHeight 0.5s linear', 
         moveHeightInfinite: 'moveHeight 0.5s linear infinite', 
+      },
+      colors: {
+        deepblue: '#255784',
+        orangered: '#ca1f1f',
+        khaki: '#92530b',
       },
       keyframes: {
         moveWidth: {
