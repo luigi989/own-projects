4b42ec5d44f58bd8b859e2ea7ef4c3d39f6b6e2c
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 <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>
2022-02-24 12:33:19 +0100

    Changed background color to better fit the page

--- Examples/src/login_forms/login_form.jsx
+++ Examples/src/login_forms/login_form.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Login_form() {
     return(
-        <div className='flex-1 flex justify-center items-center'>
-            <form className="flex flex-col bg-[#191919] p-10">
+        <div className='flex-1 flex justify-center items-center bg-[#191919]'>
+            <form className="flex flex-col p-10">
                 <h1 className='text-white text-center uppercase text-3xl'>Login</h1>
                 <input className='focus:border-blue-500 focus:rounded-none
                                   bg-[#191919] duration-300 ease-in
@@ -21,9 +21,9 @@ export default function Login_form() {
                         name="" 
                         placeholder="Password" 
                 />
-                <input className='hover:bg-green-400 w-2/3 self-center
+                <input className='hover:bg-green-400 hover:text-black w-2/3 self-center
                                     border-2 border-solid border-green-400 rounded-3xl 
-                                    px-6 py-2 text-white text-center outline-none'
+                                    px-6 py-2 text-white text-center outline-none duration-300'
                         type="submit" 
                         name=""
                         value="Login" 
