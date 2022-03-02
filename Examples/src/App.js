9799cf61b22fceddbad9d2f3ff310b23d870a3a7
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 <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>
2022-03-02 10:56:44 +0100

    Add night/light mode switch

--- Examples/src/App.js
+++ Examples/src/App.js
@@ -1,17 +1,29 @@
-function App() {
-  return (
-    <div className='relative h-screen w-screen'>
-      <div className='flex flex-row'>
-        <span className={`absolute top-1/2 left-0 h-[2px] w-0
-                          animate-moveWidthInfin bg-green-600`}>
-        </span>
-        <span className={`absolute mt-4 top-1/2 left-0 h-[2px] w-0
-                          animate-moveWidth75Infin bg-red-600`}></span>
-        <span className={`absolute mt-8 top-1/2 left-0 h-[2px] w-0
-                          animate-moveWidth50Infin bg-purple-600`}></span>
-      </div>
+import { React, useState } from 'react';
+import ModeSwitch from "./modeSwitch";
 
-      {/* <div className='flex flex-row-reverse'>
+function App() {
+  const [isDark, setIsDark] = useState(true);
+
+  return (
+    <div className={isDark ? 'dark w-full h-full' :
+      'w-full h-full'}>
+      <div className='relative w-full h-screen bg-light dark:bg-dark'>
+
+        <div className='absolute top-10 right-10'>
+          <ModeSwitch setIsDark={setIsDark} />
+        </div>
+
+        <div className='flex flex-row'>
+          <span className={`absolute top-1/2 left-0 h-[2px] w-0
+                          animate-moveWidthInfin bg-green-600`}>
+          </span>
+          <span className={`absolute mt-4 top-1/2 left-0 h-[2px] w-0
+                          animate-moveWidth75Infin bg-red-600`}></span>
+          <span className={`absolute mt-8 top-1/2 left-0 h-[2px] w-0
+                          animate-moveWidth50Infin bg-purple-600`}></span>
+        </div>
+
+        {/* <div className='flex flex-row-reverse'>
         <span className={`absolute top-1/2 left-0 h-[2px] w-0
                           animate-moveWidthInfin bg-green-600`}></span>
         <span className={`absolute mt-4 top-1/2 left-0 h-[2px] w-0
@@ -19,6 +31,7 @@ function App() {
         <span className={`absolute mt-8 top-1/2 left-0 h-[2px] w-0
                           animate-moveWidth50Infin bg-purple-600`}></span>
       </div> */}
+      </div>
     </div>
   );
 }
