82dd6b942d9ebedc07dd869c726c96d0ca7a783c
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 <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>
2022-03-03 08:46:16 +0100

    Improved user experience

--- Examples/src/login_forms/login_form.jsx
+++ Examples/src/login_forms/login_form.jsx
@@ -16,18 +16,18 @@ export default function Login_form() {
 
                 <form className="flex flex-col p-5 bg-slate-300 rounded-2xl shadow-xl">
                     <h1 className='text-light dark:text-light text-center uppercase text-3xl'>Login</h1>
-                    <div className='my-4 flex gap-5 items-center
+                    <div className='my-4 flex gap-5 items-center focus-within:bg-deepblue focus-within:text-dark
                                     border-2 border-solid border-deepblue rounded-3xl'>
                         <FaUser className='ml-3'/>
-                        <input className='py-2 pl-2 bg-transparent rounded-r-3xl duration-300
-                                        focus:bg-deepblue focus:text-dark placeholder:text-deepblue' 
-                                type='text' placeholder='User' />
+                        <input className='py-2 pl-2 bg-transparent rounded-r-3xl duration-300 outline-none
+                                        focus:text-dark placeholder:text-deepblue' 
+                                type='text' placeholder='User' required />
                     </div>
-                    <div className='my-2 flex gap-5 items-center
+                    <div className='my-2 flex gap-5 items-center focus-within:bg-deepblue focus-within:text-dark
                                     border-2 border-solid border-deepblue rounded-3xl'>
                         <FaLock className='ml-3'/>
-                        <input className='py-2 pl-2 bg-transparent rounded-r-3xl duration-300
-                                        focus:bg-deepblue focus:text-dark placeholder:text-deepblue' 
+                        <input className='py-2 pl-2 bg-transparent rounded-r-3xl duration-300 outline-none
+                                        focus:text-dark placeholder:text-deepblue' 
                                 type='password' placeholder='Password' required />
                     </div>
                     <input className='focus:bg-green-700 dark:focus:text-black
