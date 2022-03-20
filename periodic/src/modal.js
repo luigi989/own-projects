4af499b2f0e2be9cb91d764cb2f4278264b02e44
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 <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>
2022-03-20 11:55:41 +0100

    Fixed layout issues

--- periodic/src/modal.js
+++ periodic/src/modal.js
@@ -18,35 +18,23 @@ function ElementModal( {onOpen, onClose, data }) {
 
         <div className='flex justify-between'>
           {/* Facts */}
-          <ul>
-            <li>
-              <strong className='text-light'>Appearance: </strong>
-              <span className='text-slate-500'>{data.appearance}</span>
-            </li>
-            <li>
-              <strong className='text-light'>Phase: </strong>                    
-              <span className='text-slate-500'>{data.phase}</span>
-            </li>
-            <li>
-              <strong className='text-light'>Atomic mass: </strong> 
-              <span className='text-slate-500'>{data.atomic_mass} u</span>
-            </li>
-            <li>
-              <strong className='text-light'>Boil point: </strong> 
-              <span className='text-slate-500'>{data.boil} &#176;&#8490;</span>
-            </li>
-            <li>
-              <strong className='text-light'>Melting point: </strong> 
-              <span className='text-slate-500'>{data.melt} &#176;&#8490;</span>
-            </li>
-            <li>
-              <strong className='text-light'>Density: </strong> 
-              <span className='text-slate-500'>{data.density} g/cm&#179;</span>
-            </li>
+          <ul className='w-1/2'>
+            {[['Appearance', data.appearance, ''], 
+              ['Phase', data.phase, ''], 
+              ['Atomic mass', data.atomic_mass , 'u'],
+              ['Boil point', data.boil, <span>&#176;&#8490;</span>], 
+              ['Melting point', data.melt , <span>&#176;&#8490;</span>], 
+              ['Density', data.density, <span>g/cm&#179;</span>]]
+              .map(([description, text, unit]) => (
+                <li className='p-1 bg-slate-800'>
+                  <strong className='text-light' >{description}: </strong>
+                  <span className='text-slate-500'>{text} {unit}</span>
+                </li>
+              ))}
           </ul>
 
           {/* Image */}
-          <div className='flex flex-col items-center justify-center'>
+          <div className='w-1/3 h-1/3 mr-16 flex flex-col items-center justify-center'>
             <img className='w-100 p-2'
               src={ require(`./images/${data.name}.png`) }
               alt={`${data.name} atomic model`} />
@@ -69,70 +57,5 @@ function ElementModal( {onOpen, onClose, data }) {
     </Popup>
   );
 }
-
-// function ElementModal( {closeModal, data} ) {
-//     return (
-//         <>
-//           <Modal className="modal" size="lg" show={true} onHide={closeModal}>
-//             <Modal.Header className="" closeButton>
-//               <Modal.Title className="w-100 text-center text-light">
-//                 <strong>{data.name}</strong>
-//                 </Modal.Title>
-//             </Modal.Header>
-//             <Modal.Body>
-//               <Row>
-//                 <Col xs={12} md={6}>
-//                 <ListGroup>
-//                   <ListGroup.Item>
-//                     <strong className='text-light'>Appearance: </strong>
-//                     <span className='text-secondary'>{data.appearance}</span>
-//                   </ListGroup.Item>
-//                   <ListGroup.Item>
-//                     <strong className='text-light'>Phase: </strong>                    
-//                     <span className='text-secondary'>{data.phase}</span>
-//                   </ListGroup.Item>
-//                   <ListGroup.Item>
-//                     <strong className='text-light'>Atomic mass: </strong> 
-//                     <span className='text-secondary'>{data.atomic_mass} u</span>
-//                   </ListGroup.Item>
-//                   <ListGroup.Item>
-//                     <strong className='text-light'>Boil point: </strong> 
-//                     <span className='text-secondary'>{data.boil} &#176;&#8490;</span>
-//                   </ListGroup.Item>
-//                   <ListGroup.Item>
-//                     <strong className='text-light'>Melting point: </strong> 
-//                     <span className='text-secondary'>{data.melt} &#176;&#8490;</span>
-//                   </ListGroup.Item>
-//                   <ListGroup.Item>
-//                     <strong className='text-light'>Density: </strong> 
-//                     <span className='text-secondary'>{data.density} g/cm&#179;</span>
-//                   </ListGroup.Item>
-//                 </ListGroup>
-//                 </Col>
-//                 <Col xs={12} md={6} 
-//                     className="text-light d-flex flex-column 
-//                     justify-content-center align-items-center">
-//                   <img 
-//                     className='w-50 p-2'
-//                     src={ require(`./images/${data.name}.png`).default }
-//                     alt={`${data.name} atomic model`}
-//                   ></img>
-//                 </Col>
-//               </Row>
-//               <Row className="m-1 mt-3 text-light align-middle">
-//                 <hr></hr>
-//                 <span className='mb-3'>{data.summary}</span>
-//                 <hr></hr>
-//               </Row>
-
-//               <Container className="w-100 text-center text-light">
-//                 {data.category}<br/>
-//                 {data.number} | {data.period}
-//               </Container>
-//             </Modal.Body>
-//           </Modal>
-//         </>
-//       );
-// }
 
 export default ElementModal;
