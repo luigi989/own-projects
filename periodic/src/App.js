a7cffd5b0c04fcbe95d76313753e0ab2fd1ef01b
Luigi989 <ludde.lindahl@gmail.com>
2022-03-26 01:09:28 +0100

    Refactor code to work with row hover

--- periodic/src/App.js
+++ periodic/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NumberCell } from './cells';
 import { Row1, Row2, Row3, Row4, Row5, Row6, Row6exp, Row7, Row7exp } from './rows';
 
 
@@ -8,24 +9,44 @@ function App() {
       <header className="w-full flex items-center justify-center py-2 bg-slate-800">
         <span className='text-3xl text-slate-400'>Periodiska systemet</span>
       </header>
-      <div className=" flex flex-col mx-auto w-fit">
-        {[['1', <Row1 />], 
-          ['2', <Row2 />], 
-          ['3', <Row3 />], 
-          ['4', <Row4 />],
-          ['5', <Row5 />], 
-          ['6', <Row6 />], 
-          ['7', <Row7 />], 
-          ['8', <Row6exp />], 
-          ['9', <Row7exp />]]
-          .map(([number, row]) => (
-            <div className='flex' key={number}>
-              <span className='peer wh-10 flex items-center justify-center px-3 
-                                        text-white text-xl'
-                    id='row1num'>{number}</span>
-              <div className='peer-hover:grayscale'>{row}</div>
-            </div>
-          ))}
+      <div className="mt-2 mx-auto w-fit text-white">
+        <NumberCell id='row1num' number='1' />
+        <NumberCell id='row2num' number='2' />
+        <NumberCell id='row3num' number='3' />
+        <NumberCell id='row4num' number='4' />
+        <NumberCell id='row5num' number='5' />
+        <NumberCell id='row6num' number='6' />
+        <NumberCell id='row7num' number='7' />
+        <NumberCell id='row6num' number='6' />
+        <NumberCell id='row7num' number='7' />
+        
+        <div />
+            <div id='row1' className='inline-block'><Row1 /></div>
+            <div />
+
+            <div id='row2' className='inline-block'><Row2 /></div>
+            <div/>
+
+            <div id='row3'  className='inline-block'><Row3 /></div>
+            <div />
+            
+            <div id='row4' className='inline-block'><Row4 /></div>
+            <div />
+
+            <div id='row5' className='inline-block'><Row5 /></div>
+            <div/>
+
+            <div id='row6' className='inline-block'><Row6 /></div>
+            <div />
+
+            <div id='row7' className='inline-block'><Row7 /></div>
+            <div />
+
+            <div id='row6exp' className='inline-block'><Row6exp /></div>
+            <div/>
+
+            <div id='row7exp' className='inline-block'><Row7exp /></div>
+            <div/>
       </div>
     </div>
   );
