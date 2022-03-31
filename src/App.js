8d53c4c1f9c151f5a2b118466255879eb726257c
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 <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>
2022-03-31 15:23:08 +0200

    Fix sizing between components

--- src/App.js
+++ src/App.js
@@ -4,23 +4,14 @@ import { Row1, Row2, Row3, Row4, Row5, Row6, Row6exp, Row7, Row7exp } from './ro
 
 
 function App() {
-
-  function getElem() {
-    let elem = document.getElementById('row1');
-    let el = document.getElementById('row1num');
-    el.onclick = function () {
-      this.style.border = "thick solid green";
-    };
-  }
-
   return (
     <div className='background'>
       <header className="w-full flex items-center justify-center py-2 bg-slate-800">
         <span className='text-3xl text-slate-400'>Periodiska systemet</span>
       </header>
-      <div className="mt-2 mx-auto text-white flex">
+      <div className=" text-white flex justify-center mt-4">
 
-        <div className='flex flex-col'>
+        <div className='flex flex-col gap-1'>
           <NumberCell id='row1num' number='1' row='row1'/>
           <NumberCell id='row2num' number='2' row='row2'/>
           <NumberCell id='row3num' number='3' row='row3'/>
@@ -31,8 +22,8 @@ function App() {
           <NumberCell id='row6num' number='6' row='row6'/>
           <NumberCell id='row7num' number='7' row='row7'/>
         </div>
-        <div>
-          <div id='row1' className='inline-block'><Row1 /></div>
+        <div className='flex flex-col gap-1'>
+          <div id='row1'><Row1 /></div>
           <div id='row2'><Row2 /></div>
           <div id='row3'><Row3 /></div>
           <div id='row4'><Row4 /></div>
